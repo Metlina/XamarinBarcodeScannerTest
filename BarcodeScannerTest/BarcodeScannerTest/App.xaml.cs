56a2a00d08499c1d7032fe3f37c119de076660d8
TINOPETRINA74E4\tinopetrina <tinopetrina@live.com>
2017-03-13 15:00:23 +0100

    Initial commit

---
+++ BarcodeScannerTest/BarcodeScannerTest/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace BarcodeScannerTest
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Test());
+        }
+    }
+}
---
+++ BarcodeScannerTest/BarcodeScannerTest/Test.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using ZXing.Net.Mobile.Forms;
+
+namespace BarcodeScannerTest
+{
+    public partial class Test : ContentPage
+    {
+        public Test()
+        {
+            InitializeComponent();
+        }
+
+        async void ButtonClick(object sender, EventArgs e)
+        {
+            var scanPage = new ZXingScannerPage();
+
+            scanPage.OnScanResult += (result) => {
+                // Stop scanning
+                scanPage.IsScanning = false;
+
+                // Pop the page and show the result
+                Device.BeginInvokeOnMainThread(() => {
+                    Navigation.PopAsync();
+                    DisplayAlert("Scanned Barcode", result.Text, "OK");
+                });
+            };
+
+            // Navigate to our scanner page
+            await Navigation.PushAsync(scanPage);
+        }
+    }
+}
